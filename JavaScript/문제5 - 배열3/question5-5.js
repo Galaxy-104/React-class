b759f1af37e65c8503f37de7b701be2044603933
Galaxy <js_park95@naver.com>
2023-07-17 18:11:19 +0900

    JavaScript question5-5 풀이중

---
+++ JavaScript/문제5 - 배열3/question5-5.js
@@ -0,0 +1,46 @@
+const commentInput = document.getElementById('user-comment-input') // 사용자 댓글 입력창
+const userComments = document.getElementById('user-comments') // 사용자 댓글 리스트 출력
+
+const submitBtn = document.getElementById('user-comment-add') //  사용자 댓글 추가 버튼
+const comments = [] // 사용자 댓글목록 배열
+const insults = ['shit', 'fuck', 'poop', 'dumb', '새끼', '놈', '똥', '씨발', '좇', '시발', '좆', '죽어', '뒤져', '병신', '년']
+
+
+// 댓글 정보로부터 댓글에 대한 DOM 객체 생성 및 반환
+function buildElement(comment){
+   // 구현하기
+   const commentEl = document.createElement('div')
+   commentEl.className = 'comment'
+   commentEl.innerHTML = `${comment}`
+   return commentEl
+}
+// comments 배열을 이용하여 화면에 댓글목록 보여주기
+function displayComments(comments){
+    userComments.innerHTML = ''
+    // 구현하기
+    comments.forEach(function(comment){
+        userComments.appendChild(buildElement(comment))
+    })
+
+}
+
+// 입력창 초기화
+function initInput(){
+    commentInput.value = ''
+}
+
+// 댓글 추가하기
+function addComment(){
+    const comment = commentInput.value
+
+    if(comment !== ''){
+        
+        comments.push(comment)
+        initInput()
+        displayComments(comments)
+    }else{
+        alert('You need to give a comment !')
+    }
+}
+
+submitBtn.addEventListener('click', addComment)
