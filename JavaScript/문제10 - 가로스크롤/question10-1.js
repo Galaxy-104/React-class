bbbb9b0d0da72d577ce8dd2fa460ad9dadf3314c
Galaxy <js_park95@naver.com>
2023-07-25 18:12:22 +0900

    JavaScript question10-1 아이템정보 랜덤으로 가져오기

--- JavaScript/문제10 - 가로스크롤/question10-1.js
+++ JavaScript/문제10 - 가로스크롤/question10-1.js
@@ -10,7 +10,7 @@ const modalContainer = document.querySelector('.modal-container')
 const items = itemContainer.querySelectorAll('.item')
 const selectedItems = modalContainer.querySelector('.selected-items')
 
-const openBtn = document.querySelector('.buttons button:nth-child(2)')
+const usersBookmarkBtn = document.querySelector('.buttons .users-bookmark')
 const closeBtn = modalContainer.querySelector('button')
 
 let startX
@@ -18,24 +18,82 @@ let isDown = false
 let scrollLeft
 let isClicked = false
 
+
 fetch(API_URL)
 .then(response => response.json())
-.then(products => console.log(products))
+.then(function(products){
 
-//  아이템 div 생성
-let j = 65
-for(let i = 0; i < 20; i++){
+    //  아이템 div 생성
+
+    // 10개의 랜덤 인덱스 생성
+    function pickIndex(len){
+        return Math.floor(Math.random() * len)
+    }
     
-    const div = document.createElement('div')
-    div.className = 'item'
-    div.innerHTML = `<button class="bookmark">
-    <span class="material-symbols-rounded">
-    star
-    </span>
-    </button>`
-    j++
-    itemContainer.appendChild(div)
-}
+    const pickRandomIndex = []
+
+    for(let i = 0; i < 10; i++){
+
+        // 제품을 랜덤으로 선택
+        let randomIndex = pickIndex(products.length)
+        
+        while(pickRandomIndex.includes(randomIndex)){
+            randomIndex = pickIndex(products.length)
+        }
+        pickRandomIndex.push(randomIndex)
+
+        // 제품 평점이 없는 경우
+        if(products[pickRandomIndex[i]].rating === null){
+            products[pickRandomIndex[i]].rating = ' '
+        }
+
+        // 제품 이름에 브랜드 이름 제거
+        let productName = products[pickRandomIndex[i]].name.split(' ').splice(1).join(' ')
+
+        // 랜덤 인덱스의 정보를 가지고 와서 div 만들기
+        const div = document.createElement('div')
+        div.className = 'item'
+        div.innerHTML = `
+        <div class="item-img">
+            <img src="${products[pickRandomIndex[i]].image_link}" alt="">
+            <div class="item-colors"></div>
+        </div>
+        <div class="item-info">
+            <span>${productName}</span>
+            <span>
+                <span class="bold">Price </span>
+                ${products[pickRandomIndex[i]].price}
+            </span>
+            <span>
+                <span class="bold">Type </span>
+                ${products[pickRandomIndex[i]].product_type}
+            </span>
+            <span class="rating">
+                <span class="material-symbols-rounded">
+                    star
+                </span>
+                ${(products[pickRandomIndex[i]].rating)}
+            </span>
+        </div>
+        <button class="bookmark">
+            <span class="material-symbols-rounded">
+                thumb_up
+            </span>
+        </button>`
+        itemContainer.appendChild(div)
+
+        // 제품의 색상 정보 만들기
+
+        let productColors = products[pickRandomIndex[i]].product_colors
+        console.log(productColors)
+        
+        
+
+    }
+
+})
+
+
 
 
 
@@ -82,8 +140,9 @@ itemContainer.addEventListener('click', (e) => {
 
 
 
-openBtn.addEventListener('click', () => {
+usersBookmarkBtn.addEventListener('click', () => {
     modalContainer.style.display = 'block'
+    modalContainer.style.zIndex = '3'
 })
 closeBtn.addEventListener('click', () => {
     modalContainer.style.display = 'none'
