3b93b715e298a125d15610dbbde200d15cf08f62
Galaxy <js_park95@naver.com>
2023-07-27 16:05:47 +0900

    JS question10-1 무한스크롤 적용 완료

--- JavaScript/문제10 - 가로스크롤/question10-1.js
+++ JavaScript/문제10 - 가로스크롤/question10-1.js
@@ -150,66 +150,83 @@ fetch(API_URL)
 
     const cardContainer = main.querySelector('.card-container')
     
-    products.forEach((product, index) => {
-        
-        // 제품 평점이 없는 경우
-        if(product.rating === null){
-            product.rating = ' '
-        }
+    let offset = 0
+    function createEightItems(offset){
+        for(let i = offset; i < offset + 8 && i < products.length; i++){
+            let product = products[i]
+            // 제품 평점이 없는 경우
+            if(product.rating === null){
+                product.rating = ' '
+            }
 
-        // 제품 이름에 브랜드 이름 제거
-        let productName = product.name.split(' ').splice(1).join(' ')
+            // 제품 이름에 브랜드 이름 제거
+            let productName = product.name.split(' ').splice(1).join(' ')
 
-        const cardDiv = document.createElement('div')
-        cardDiv.className = 'card-item'
-        cardDiv.innerHTML = `
-        <div class="item-img">
-            <img src="${product.image_link}" alt="">
-        </div>
+            const cardDiv = document.createElement('div')
+            cardDiv.className = 'card-item'
+            cardDiv.innerHTML = `
+            <div class="item-img">
+                <img src="${product.image_link}" alt="">
+            </div>
 
-        <div class="item-colors"></div>
+            <div class="item-colors"></div>
 
-        <div class="item-info">
-            <span>${productName}</span>
-            <span>
-                <span class="bold">Price </span>
-                ${product.price}
-            </span>
-            <span>
-                <span class="bold">Type </span>
-                ${product.product_type}
-            </span>
-            <span class="rating">
-                <span class="material-symbols-rounded">
-                    star
+            <div class="item-info">
+                <span>${productName}</span>
+                <span>
+                    <span class="bold">Price </span>
+                    ${product.price}
                 </span>
-                ${(product.rating)}
-            </span>
-        </div>
-        <button class="bookmark">
-            <span class="material-symbols-rounded">
-                thumb_up
-            </span>
-        </button>`
-        cardContainer.appendChild(cardDiv)
+                <span>
+                    <span class="bold">Type </span>
+                    ${product.product_type}
+                </span>
+                <span class="rating">
+                    <span class="material-symbols-rounded">
+                        star
+                    </span>
+                    ${(product.rating)}
+                </span>
+            </div>
+            <button class="bookmark">
+                <span class="material-symbols-rounded">
+                    thumb_up
+                </span>
+            </button>`
+            cardContainer.appendChild(cardDiv)
 
-        // 제품의 색상 정보 만들기
-        const cardItems = main.querySelectorAll('.card-item')
-        const carditemColor = cardItems[index].querySelector('.item-colors')
-        let productColors = product.product_colors
+            // 제품의 색상 정보 만들기
+            const cardItems = main.querySelectorAll('.card-item')
+            const carditemColor = cardItems[i].querySelector('.item-colors')
+            let productColors = product.product_colors
 
-        if(productColors.length !== 0){
-            for(let i = 0; i < productColors.length; i++){
-                const colorDiv = document.createElement('div')
-                colorDiv.className = 'item-color'
-                colorDiv.style.background = `${productColors[i].hex_value}`
+            if(productColors.length !== 0){
+                for(let i = 0; i < productColors.length; i++){
+                    const colorDiv = document.createElement('div')
+                    colorDiv.className = 'item-color'
+                    colorDiv.style.background = `${productColors[i].hex_value}`
 
-                carditemColor.appendChild(colorDiv)
+                    carditemColor.appendChild(colorDiv)
+                }
             }
         }
-    })
+    }
+    createEightItems(offset)
     
+    window.addEventListener('scroll', (event) => {
+        const scrollHeight = Math.max(   // 전체문서 높이 (스크롤이벤트 내부에 있어야 함)
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+        );
 
+        if(Math.abs(window.pageYOffset + document.documentElement.clientHeight - scrollHeight) < 100){
+            console.log('@@')
+            offset = offset + 8
+            createEightItems(offset)
+        }
+
+    })
 
 })
 
